1f655eb4674813bfc1fb9cdedafad66b242c4d15
Tyler Cozzocrea <tylercozzocrea@gmail.com>
2025-04-04 16:39:25 -0700

    makes multiple threads that print a number 0-4

--- 01/project/hellothread.c
+++ 01/project/hellothread.c
@@ -1,3 +1,28 @@
+#include <stdio.h>
+#include <pthread.h>
+#include <unistd.h>
+
+void *run(void *arg) {
+  int *thread_number = arg;
+  printf("thread %d: 0\n", *thread_number);
+  printf("thread %d: 1\n", *thread_number);
+  // On my machine, it will print the first thread and then the second,
+  // which qustions whether the threads are concurrent
+  // the sleep shows that the threads are indeed concurrent.
+  usleep(10000);
+  printf("thread %d: 2\n", *thread_number);
+  printf("thread %d: 3\n", *thread_number);
+  printf("thread %d: 4\n", *thread_number);
+  return NULL;
+}
+
 int main() {
-  return 0;
+  printf("launching threads\n");
+  pthread_t t1;
+  pthread_t t2;
+  pthread_create(&t1, NULL, run, &(int){1});
+  pthread_create(&t2, NULL, run, &(int){2});
+  pthread_join(t1, NULL);
+  pthread_join(t2, NULL);
+  printf("Threads complete!\n");
 }
